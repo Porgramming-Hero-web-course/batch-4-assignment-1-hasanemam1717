ed3d1610ee97087261e3530ada847bbb7b18cc07
hasanemam1717 <hasanemam1717@gmail.com>
2024-11-06 20:52:11 +0600

    perfectlly solved problem 2

---
+++ problem-2.ts
@@ -0,0 +1,8 @@
+{
+    // Problem =2;
+    function removeDuplicates(numbers: number[]): number[] {
+        return numbers.filter((element, index) => numbers.indexOf(element) === index);
+    }
+
+    console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+}
